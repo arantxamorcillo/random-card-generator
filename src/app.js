1541b94a38d54a28d7830dd0e62d51e1827b2c8b
arantxamorcillo <arantxa.morcillo@gmail.com>
2021-08-09 14:14:28 +0000

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 /* eslint-disable */
-import "bootstrap";
+
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let cardNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, "10", "👸", "👑"];
+
+  let cardSuits = ["♥️", "♦️", "♣️", "♠️"];
+
+  function generateRandomNumber(posibilities) {
+    let randomNumber = Math.floor(Math.random() * posibilities.length);
+    //console.log(randomNumber);
+    return posibilities[randomNumber];
+  }
+
+  let number = document.querySelector(".number");
+  let cardNumber = generateRandomNumber(cardNumbers);
+  number.innerHTML = cardNumber;
+
+  let suit = document.getElementsByClassName("suit");
+
+  let cardSuit = generateRandomNumber(cardSuits);
+  for (let i = 0; i < suit.length; i++) {
+    suit[i].innerHTML = cardSuit;
+  }
 };
