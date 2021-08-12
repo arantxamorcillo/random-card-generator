b0dd9deecf78f1ddeb56b8d0a07048155396e244
arantxamorcillo <arantxa.morcillo@gmail.com>
2021-08-12 09:28:32 +0000

    diferrent aproach and added button

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 /* eslint-disable */
 
+import { Button } from "bootstrap";
 import "./style.css";
 
-window.onload = function() {
-  let cardNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, "10", "👸", "👑"];
+window.onload = getRandomCard;
+let button = document.querySelector("button");
+console.log(button);
+button.onclick = getRandomCard;
 
-  let cardSuits = ["♥️", "♦️", "♣️", "♠️"];
+function getRandomCard(ev) {
+  console.log(ev);
+  let cardNumbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K"];
+  let Suits = ["diamonds", "clubs", "hearts", "spades"];
 
   function generateRandomNumber(posibilities) {
     let randomNumber = Math.floor(Math.random() * posibilities.length);
@@ -13,14 +19,21 @@ window.onload = function() {
     return posibilities[randomNumber];
   }
 
+  let suit = generateRandomNumber(Suits);
+  let card = document.querySelector(".card");
+  let cardclass = card.classList;
+  cardclass.remove(cardclass[1]);
+  cardclass.add(suit);
+  //console.log(cardclass);
+
   let number = document.querySelector(".number");
   let cardNumber = generateRandomNumber(cardNumbers);
   number.innerHTML = cardNumber;
 
-  let suit = document.getElementsByClassName("suit");
-
-  let cardSuit = generateRandomNumber(cardSuits);
-  for (let i = 0; i < suit.length; i++) {
-    suit[i].innerHTML = cardSuit;
-  }
-};
+  //let cardSuits = ["♥️", "♦️", "♣️", "♠️"];
+  //   let suit = document.getElementsByClassName("suit")
+  //   let cardSuit = generateRandomNumber(cardSuits);
+  //   for (let i = 0; i < suit.length; i++) {
+  //     suit[i].innerHTML = cardSuit;
+  //   }
+}
